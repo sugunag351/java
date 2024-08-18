3d43c65cdfacdd423ce467a4637c5add1c5bd590
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-08-18 19:46:28 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package WrapperClasses;
+
+import com.sun.jdi.FloatValue;
+
+public class main
+{
+    public static void main(String args[])
+    {
+        Integer a=Integer.reverse(138);
+        System.out.println(a);
+    }
+}
