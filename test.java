942df040b7baa713896e28f40480b4d2e83f9791
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-09-06 22:28:33 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,68 +1,17 @@
-package abstractClassPractice;
-abstract class shape
-{
-   abstract double perimeter();
-   abstract double  area();
-}
-class circle extends shape{
-    double radius;
-    circle()
-    {
-        radius=1;
-    }
-    circle(int r)
-    {
-        radius=r;
-    }
-    double  perimeter()
-    {
-        return 2*Math.PI*radius;
-    }
-    double area()
-    {
-        return Math.PI*radius*radius;
-    }
-}
-class rectangle extends shape{
-    double length;
-    double breadth;
-    rectangle()
-    {
-        length=1;
-        breadth=1;
-    }
-    rectangle(int l,int b)
-    {
-        length=l;
-        breadth=b;
-    }
-    double perimeter()
-    {
-        return 2*(length+breadth);
-    }
-    double area()
-    {
-        return length*breadth;
-    }
-}
+package ByteArrayStreams;
+import java.io.*;
 public class test
 {
-    public static void main(String args[])
+    public static void main(String args[])throws Exception
     {
-       shape c=new circle();
-       System.out.println(c.perimeter());
-       System.out.println(c.area());
+        FileReader fos=new FileReader("c:/users/dell/myjava/my.txt");
+        BufferedReader bis=new BufferedReader(fos);
+            System.out.println(bis.readLine());
 
-       shape c1=new circle(10);
-       System.out.println(c1.perimeter());
-       System.out.println(c1.area());
 
-        shape r=new rectangle();
-        System.out.println(r.perimeter());
-        System.out.println(r.area());
 
-        shape r1=new rectangle(10,20);
-        System.out.println(r1.perimeter());
-        System.out.println(r1.area());
+
+
+
     }
 }
