ad5c8cd708fd27c7194863d5c2bab63705b01823
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-09-23 13:43:08 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,16 +1,31 @@
-package ByteArrayStreams;
-import java.io.*;
-public class test
+package enums;
+enum dept
 {
-    public static void main(String args[])throws Exception
+    CS("john","block A"),IT("suguna","block B"),CIVIL("sindhu","block C"),ECE("sounjanya","block d");
+   String name;
+    String location;
+    dept(String name,String location)
     {
-        FileReader fos=new FileReader("c:/users/dell/myjava/my.txt");
-        BufferedReader bis=new BufferedReader(fos);
-            System.out.println(bis.readLine());
+        this.name=name;
+        this.location=location;
+    }
+    public String getname()
+    {
+        return name;
+    }
+    public String getLocation()
+    {
+        return location;
+    }
 
+}
+public class test {
+    public static void main(String a[])
+    {
 
-
-
+      dept d=dept.IT;
+      System.out.println(d.getname());
+        System.out.println(d.getLocation());
 
 
     }
