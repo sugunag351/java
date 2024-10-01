37e675408934a925aefd87a3a62a8d4b5e1c3736
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-10-01 13:02:17 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,18 +1,73 @@
-package fileClass;
+package InterThreadCommunictaion;
+class mydata
+{
+    int value;
+   boolean flag=true;
+    synchronized public void set(int v)
+    {
+        while(flag!=true)
+          try{wait();}catch(Exception e){}
+        value=v;
+        flag=false;
+        notify();
+    }
+  synchronized public int get()
+    {
+        int x=0;
+       while(flag!=false)
+           try{wait();}catch(Exception e){}
+        x=value;
+         flag=true;
+         notify();
+        return x;
+    }
+}
+class producer extends Thread
+{
+    mydata d;
+    public producer(mydata d)
+    {
+        this.d=d;
+    }
+    public void run()
+    {
+         int i=1;
+        while(true)
+        {
+            d.set(i);
+             System.out.println("producer:"+i);
+             try{Thread.sleep(1000);}catch(Exception e){}
+             i++;
+        }
+    }
+}
+class consumer extends Thread
+{
+    mydata d;
+    public consumer(mydata d)
+    {
+        this.d=d;
+    }
+    public void run()
+    {
+        int value;
+        while(true)
+        {
+            value=d.get();
+            System.out.println("consumer:"+value);
+            try{Thread.sleep(1000);}catch(Exception e){}
 
-import java.io.File;
-
+        }
+    }
+}
 public class test
 {
     public static void main(String args[])
     {
-        File f=new File("c:/users/dell/myjava");
-        System.out.println(f.isDirectory());
-        String list[]=f.list();
-        for(String x:list)
-        {
-            System.out.println(x);
-        }
-
+        mydata data=new mydata();
+        producer p=new producer(data);
+        consumer c=new consumer(data);
+        p.start();
+        c.start();
     }
 }
