9fea1f563e095011fc9d35eab9486607e64de872
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-10-04 20:52:10 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,73 +1,29 @@
-package InterThreadCommunictaion;
-class mydata
+package LambdaExpressions;
+interface mylambda
 {
-    int value;
-   boolean flag=true;
-    synchronized public void set(int v)
+    public void  display();
+}
+class uselambda
+{
+    public void calllambda(mylambda ml)
     {
-        while(flag!=true)
-          try{wait();}catch(Exception e){}
-        value=v;
-        flag=false;
-        notify();
-    }
-  synchronized public int get()
-    {
-        int x=0;
-       while(flag!=false)
-           try{wait();}catch(Exception e){}
-        x=value;
-         flag=true;
-         notify();
-        return x;
+        ml.display();
     }
 }
-class producer extends Thread
+class demo
 {
-    mydata d;
-    public producer(mydata d)
+    public void method1()
     {
-        this.d=d;
+       uselambda ul=new uselambda();
+       ul.calllambda(()->{System.out.println("hello");});
     }
-    public void run()
-    {
-         int i=1;
-        while(true)
-        {
-            d.set(i);
-             System.out.println("producer:"+i);
-             try{Thread.sleep(1000);}catch(Exception e){}
-             i++;
-        }
-    }
-}
-class consumer extends Thread
-{
-    mydata d;
-    public consumer(mydata d)
-    {
-        this.d=d;
-    }
-    public void run()
-    {
-        int value;
-        while(true)
-        {
-            value=d.get();
-            System.out.println("consumer:"+value);
-            try{Thread.sleep(1000);}catch(Exception e){}
 
-        }
-    }
 }
 public class test
 {
     public static void main(String args[])
     {
-        mydata data=new mydata();
-        producer p=new producer(data);
-        consumer c=new consumer(data);
-        p.start();
-        c.start();
+       demo d=new demo();
+       d.method1();
     }
 }
