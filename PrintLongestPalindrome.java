48cf30a583f8d7d98473b76d0ca8d74e4f0ec651
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2024-11-03 11:06:59 +0530

    Add files via upload

---
+++ PrintLongestPalindrome.java
@@ -0,0 +1,45 @@
+package programming21;
+
+public class PrintLongestPalindrome
+{
+    static boolean isPalindrome(String t)
+    {
+        int i=0,j=t.length()-1;
+        while(i<j)
+        {
+            if(t.charAt(i)==t.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void meth(String s)
+    {
+        for(int n=s.length();n>0;n--)
+        {
+            for (int i = 0; i <= s.length() - n; i++)
+            {
+                String t = "";
+                for (int j = i; j < n + i; j++)
+                {
+                    t = t + s.charAt(j);
+                }
+                if(isPalindrome(t)) {
+                    System.out.println(t);
+                    return;
+                }
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        String s="mississippi";
+        meth(s);
+    }
+}
