53161080d8e15f5c565834b468a6b238088d3343
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2025-01-29 20:21:46 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,21 +1,44 @@
-package SequenceInputStream;
+package serialization;
 import java.io.*;
+class student implements Serializable
+{
+   private int rollno;
+   private String name;
+   private float avg;
+    private String dept;
+    public static int data=10;
+    public transient int t;
+    public student()
+    {
+
+    }
+    public student(int r,String n,float a,String d)
+    {
+        rollno=r;
+        name=n;
+        dept=d;
+        avg=a;
+        data=500;
+        t=500;
+    }
+    public String toString()
+    {
+        return "student details:"+
+                "\nroll:"+rollno+
+                "\nNmae:"+name+
+                "\nAverage:"+avg+
+                "\nDept:"+dept+
+                "\nData:"+data+
+                "\nTransient:"+t+"\n";
+    }
+}
 public class test
 {
     public static void main(String args[])throws Exception
     {
-        FileInputStream fis1=new FileInputStream("c:/users/dell/myjava/my.txt");
-        FileInputStream fis2=new FileInputStream("c:/users/dell/myjava/source4.txt");
-        FileOutputStream fos=new FileOutputStream("c:/users/dell/myjava/destination.txt");
-        SequenceInputStream sis=new SequenceInputStream(fis1,fis2);
-        try(fis1;fis2;fos;sis)
-        {
-            int x;
-            while((x=sis.read())!=-1)
-            {
-                fos.write(x);
-            }
-        }
-
+       FileInputStream fos=new FileInputStream("c:/users/dell/myjava/his.txt");
+       ObjectInputStream oos=new ObjectInputStream(fos);
+       student s=(student)oos.readObject();
+       System.out.println(s);
     }
 }
