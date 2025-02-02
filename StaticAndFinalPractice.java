8795dcfb7e399a510d8bb2e62388b755db09c908
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2025-02-02 10:44:52 +0530

    Add files via upload

---
+++ StaticAndFinalPractice.java
@@ -0,0 +1,18 @@
+package StaticAndFinalPractice;
+final class my
+{
+    final int x=10;
+    final void show()
+    {
+        System.out.println(x);
+    }
+}
+public class StaticAndFinalPractice
+{
+    public static void main(String args[])
+    {
+      my i=new my();
+      i.show();
+    }
+}
+
