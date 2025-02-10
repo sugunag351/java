a01a38686d086a6fc0cbbf6fcbfbdcd4e586c0c8
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2025-02-10 21:46:23 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,18 +1,80 @@
-package String_StringBuffer_StringBuilder;
-
-public class test
+package TeacherStudentsMultithreading;
+class whiteboard
 {
-    public static  void  main(String args[])
+    String text;
+    int count=0;
+    int noofstudents=0;
+    public void attendance()
     {
-        String s1=new String("hello");
-        StringBuffer s2=new StringBuffer("hello");
-        StringBuilder s3=new StringBuilder("hello");
-        s1.concat(" world");
-        s2.append(" world");
-        s3.append(" world");
-        System.out.println(s1);//immutable
-        System.out.println(s2);//mutable
-        System.out.println(s3);//mutable
+        noofstudents++;
+    }
+    synchronized public void write(String t)
+    {
+        System.out.println("teacher is writing:"+t);
+        while(count!=0)
+            try{wait();}catch(Exception e){}
+        text=t;
+        count=noofstudents;
+        notifyAll();
+    }
+    synchronized public String read()
+    {
+        while(count==0)
+            try{wait();}catch(Exception e){}
+        String t=text;
+        count--;
+        if(count==0)
+            notify();
+        return t;
+    }
+}
+class teacher extends Thread
+{
+    whiteboard wb;
+    String notes[]={"1","2","3","end"};
+    public teacher(whiteboard w)
+    {
+        wb=w;
+    }
+    public void run()
+    {
+        for(int i=0;i<notes.length;i++)
+            wb.write(notes[i]);
+    }
+}
+class student extends Thread{
+    whiteboard wb;
+    String name;
+    public student(String n,whiteboard w)
+    {
+        name=n;
+        wb=w;
+    }
+    public void run()
+    {
+        String text;
+        wb.attendance();
+        do {
+             text=wb.read();
+            System.out.println(name + "reading:  " +text);
+        }while(!text.equals("end"));
+
+    }
+}
+public class test {
+    public static void main(String args[])
+    {
+        whiteboard wb=new whiteboard();
+        teacher t=new teacher(wb);
+        student s1=new student("a: ",wb);
+        student s2=new student("b: ",wb);
+        student s3=new student("c: ",wb);
+        student s4=new student("f: ",wb);
+        t.start();
+        s1.start();
+        s2.start();
+        s3.start();
+        s4.start();
 
     }
 }
