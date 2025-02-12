f8941f97cbd0f462286093b0e6b655416fd58dfc
sugunag351 <168339766+sugunag351@users.noreply.github.com>
2025-02-12 17:25:31 +0530

    Add files via upload

--- test.java
+++ test.java
@@ -1,17 +1,72 @@
-package UserDefinedAnnotations;
-@interface myanno
+package userdefinedStackClassPractice;
+
+class stackoverflowException extends RuntimeException
 {
-    String name();
-    String project();
-    String date() default "today";
-    String version() default "13";
-}
-@myanno(name="ajay",project = "bank")
-public class test
-{
-    int data;
-    public static void main(String args[])
+    public String toString()
     {
-        int x;
+        return "your stack is full";
+    }
+
+}
+class stackunderflowException extends RuntimeException
+{
+    public String toString()
+    {
+       return "your stack is empty";
+    }
+
+}
+class stack
+{
+    int size;
+    int top=-1;
+    int s[];
+    public stack(int sz)
+    {
+       size=sz;
+       s=new int[sz];
+    }
+    public void push(int x)throws stackoverflowException
+    {
+        if(top==size-1)
+        {
+            throw new stackoverflowException();
+        }
+        else {
+            top++;
+            s[top]=x;
+        }
+    }
+    public void pop()throws stackunderflowException
+    {
+
+        if(top==-1)
+        {
+            throw new stackunderflowException();
+        }
+        else {
+
+            top--;
+            System.out.println(s[top]);
+        }
     }
 }
+
+public class test {
+    public static void main(String args[])
+    {
+       stack a=new stack(5);
+       try {
+           a.pop();
+       }
+       catch(stackunderflowException e)
+       {
+           System.out.println(e);
+       }
+
+
+
+    }
+
+}
+
